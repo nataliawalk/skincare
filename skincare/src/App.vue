3aea18a48a1d2a907c42b0d8a39c27686c658849
Natalia Walkiewicz <na.walkiewicz@gmail.com>
2023-11-14 13:13:14 +0100

    add vuetify

---
+++ skincare/src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+const date = ref(null)
+const formattedDate = ref(null);
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+watch(date, (newDate) => {
+  if (newDate) {
+    const selectedDate = new Date(newDate);
+    const day = selectedDate.getDay();
+    const date = selectedDate.getDate();
+    const month = selectedDate.getMonth()+1;
+    const year = selectedDate.getFullYear();
+    const monthName = monthNames[month];
+    const dayName = dayNames[day];
+
+    formattedDate.value = `${dayName} / ${date} / ${month} / ${year}`;
+
+  }
+});
+</script>
+
+<template>
+
+  <div>{{formattedDate}}</div>
+  
+  <v-container>
+    <v-row justify="space-around">
+      <v-date-picker v-model="date" elevation="24"></v-date-picker>
+    </v-row>
+  </v-container>
+
+
+
+  <v-icon icon="mdi-home" />
+  <v-btn class="red white--text">click me</v-btn>
+
+
+</template>
+
